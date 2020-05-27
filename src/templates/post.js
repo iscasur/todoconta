0f46b1b13dc4495bffe7a9f74ae9948b9ab8f7d9
Israel Castro <israel.castro@gmail.com>
2020-05-27 14:09:52 -0500

    [Update] sharing post

--- src/templates/post.js
+++ src/templates/post.js
@@ -4,6 +4,7 @@ import { graphql } from 'gatsby'
 import Helmet from 'react-helmet'
 import { Tags } from '@tryghost/helpers-gatsby'
 import { DiscussionEmbed } from 'disqus-react'
+import { InlineShareButtons } from 'sharethis-reactjs'
 
 import { Layout } from '../components/common'
 import { MetaData } from '../components/common/meta'
@@ -68,17 +69,48 @@ const Post = ({ data, location }) => {
                             />
                         </section>
 
-                        <section className="post-comments">
-                        <DiscussionEmbed
-                            shortname='todoconta'
+                        <section className="post-share">
+                        <InlineShareButtons
                             config={ {
-                                url: post.url,
-                                identifier: post.id,
-                                title: post.title,
+                                alignment: 'center',    // alignment of buttons (left, center, right)
+                                color: 'social',      // set the color of buttons (social, white)
+                                enabled: true,        // show/hide buttons (true, false)
+                                font_size: 14,        // font size for the buttons
+                                labels: 'cta',        // button labels (cta, counts, null)
+                                language: 'es',       // which language to use (see LANGUAGES)
+                                networks: [           // which networks to include (see SHARING NETWORKS)
+                                    'facebook',
+                                    'twitter',
+                                    'whatsapp'
+                                ],
+                                padding: 12,          // padding within buttons (INTEGER)
+                                radius: 4,            // the corner radius on each button (INTEGER)
+                                show_total: true,
+                                size: 40,             // the size of each button (INTEGER)
+
+                                // OPTIONAL PARAMETERS
+                                url: post.url, // (defaults to current url)
+                                title: post.title,            // (defaults to og:title or twitter:title)
+                                message: post.excerpt,     // (only for email sharing)
+                                subject: post.title,  // (only for email sharing)
+                                username: 'todoconta' // (only for twitter sharing)
                             } }
                         />
                         </section>
+                        
                     </article>
+                    <aside className="comments">
+                        <section className="post-comments">
+                            <DiscussionEmbed
+                                shortname='todoconta'
+                                config={ {
+                                    url: post.url,
+                                    identifier: post.id,
+                                    title: post.title,
+                                } }
+                            />
+                        </section>
+                    </aside>
                 </div>
             </Layout>
         </>
