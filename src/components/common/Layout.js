e0469fff3425633bb0df293b51cc92a06421e0a5
Israel Castro <israel.castro@gmail.com>
2021-01-06 14:05:53 -0600

    [Change] post layout and header

--- src/components/common/Layout.js
+++ src/components/common/Layout.js
@@ -14,14 +14,14 @@ import googleAnalytics from '@analytics/google-analytics'
 import '../../styles/app.css'
 
 const analytics = Analytics({
-    app: 'todoconta',
-    plugins: [
-      googleAnalytics({
-        trackingId: 'UA-138017682-1'
-      })
-    ]
-  })
-  
+	app: 'todoconta',
+	plugins: [
+		googleAnalytics({
+			trackingId: 'UA-138017682-1'
+		})
+	]
+})
+
 
 /**
 * Main layout component
@@ -32,104 +32,106 @@ const analytics = Analytics({
 *
 */
 const DefaultLayout = ({ data, children, bodyClass, isHome }) => {
-    const site = data.allGhostSettings.edges[0].node
-    const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
-    const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
+	const site = data.allGhostSettings.edges[0].node
+	const twitterUrl = site.twitter ? `https://twitter.com/${site.twitter.replace(/^@/, ``)}` : null
+	const facebookUrl = site.facebook ? `https://www.facebook.com/${site.facebook.replace(/^\//, ``)}` : null
 
-    return (
-        <>
-            <Helmet>
-                <html lang={site.lang} />
-                <style type="text/css">{`${site.codeinjection_styles}`}</style>
-                <body className={bodyClass} />
-            </Helmet>
+	return (
+		<>
+			<Helmet>
+				<html lang={site.lang} />
+				<style type="text/css">{`${site.codeinjection_styles}`}</style>
+				<body className={bodyClass} />
+			</Helmet>
 
-            <div className="viewport">
+			<div className="viewport">
 
-                <div className="viewport-top">
-                    {/* The main header section on top of the screen */}
-                    <header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
-                        <div className="container">
-                            <div className="site-mast">
-                                <div className="site-mast-left">
-                                    <Link to="/">
-                                        <div class="site-header-top">
-                                            {/* site.logo ?
+				<div className="viewport-top">
+					{/* The main header section on top of the screen */}
+					<header className="site-head" style={{ ...site.cover_image && { backgroundImage: `url(${site.cover_image})` } }}>
+						<div className="container">
+							<div className="site-mast">
+								<div className="site-mast-left">
+									<Link to="/">
+										<div class="site-header-top">
+											<img height="48" src="https://todoconta.s3-us-west-1.amazonaws.com/2020/05/isotipo.png" alt={site.description} />
+											{/* site.logo ?
                                                 <img className="site-logo" src={site.logo} alt={site.title} />
                                                 : <Img fixed={data.file.childImageSharp.fixed} alt={site.title} />
                                             */}
-                                            <div className="site-header-link">
-                                                <h1 className="site-banner-title">{site.title}</h1>
-                                                <p className="site-banner-desc">{site.description}</p>
-                                            </div>
-                                        </div>
-                                    </Link>
-                                </div>
-                                <div className="site-mast-right">
-                                    { site.twitter && <a href={ twitterUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
-                                    { site.facebook && <a href={ facebookUrl } className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
-                                    <a className="site-nav-item" href={ `https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/` } target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a>
-                                </div>
-                            </div>
-                            { /* isHome ?
+											<div className="site-header-link">
+												<h1 className="site-banner-title">{site.title}</h1>
+												<p className="site-banner-desc">{site.description}</p>
+											</div>
+										</div>
+									</Link>
+								</div>
+								<div className="site-mast-right">
+									{/* {site.twitter && <a href={twitterUrl} className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/twitter.svg" alt="Twitter" /></a>}
+                                    {site.facebook && <a href={facebookUrl} className="site-nav-item" target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/facebook.svg" alt="Facebook" /></a>}
+                                    <a className="site-nav-item" href={`https://feedly.com/i/subscription/feed/${config.siteUrl}/rss/`} target="_blank" rel="noopener noreferrer"><img className="site-nav-icon" src="/images/icons/rss.svg" alt="RSS Feed" /></a> */}
+									<Navigation data={site.navigation} navClass="site-nav-item" />
+								</div>
+							</div>
+							{ /* isHome ?
                                 <div className="site-banner">
                                     <h1 className="site-banner-title">{site.title}</h1>
                                     <p className="site-banner-desc">{site.description}</p>
                                 </div> :
-                                null*/} 
-                            <nav className="site-nav">
-                                <div className="site-nav-left">
-                                    {/* The navigation items as setup in Ghost */}
-                                    <Navigation data={site.navigation} navClass="site-nav-item" />
-                                </div>
-                                <div className="site-nav-right">
-                                    <Link className="site-nav-button" to="/acerca">Acerca de</Link>
-                                    <Link className="site-nav-button" to="/suscribete">Suscribete</Link>
-                                </div>
-                            </nav>
-                        </div>
-                    </header>
+                                null*/}
+							<nav className="site-nav">
+								<div className="site-nav-left">
+									{/* The navigation items as setup in Ghost */}
+									{/* <Navigation data={site.navigation} navClass="site-nav-item" /> */}
+								</div>
+								<div className="site-nav-right">
+									{/* <Link className="site-nav-button" to="/acerca">Acerca de</Link>
+                                    <Link className="site-nav-button" to="/suscribete">Suscribete</Link> */}
+								</div>
+							</nav>
+						</div>
+					</header>
 
-                    <main className="site-main">
-                        {/* All the main content gets inserted here, index.js, post.js */}
-                        {children}
-                    </main>
+					<main className="site-main">
+						{/* All the main content gets inserted here, index.js, post.js */}
+						{children}
+					</main>
 
-                </div>
+				</div>
 
-                <div className="viewport-bottom">
-                    {/* The footer at the very bottom of the screen */}
-                    <footer className="site-foot">
-                        <div className="site-foot-nav container">
-                            <div className="site-foot-nav-left">
-                                <Link to="/">{site.title}</Link> © 2020
+				<div className="viewport-bottom">
+					{/* The footer at the very bottom of the screen */}
+					<footer className="site-foot">
+						<div className="site-foot-nav container">
+							<div className="site-foot-nav-left">
+								<Link to="/">{site.title}</Link> – {site.description} © 2020
                             </div>
-                            <div className="site-foot-nav-right">
-                                <Navigation data={site.navigation} navClass="site-foot-nav-item" />
-                            </div>
-                        </div>
-                    </footer>
+							<div className="site-foot-nav-right">
+								<Navigation data={site.navigation} navClass="site-foot-nav-item" />
+							</div>
+						</div>
+					</footer>
 
-                </div>
-            </div>
+				</div>
+			</div>
 
-        </>
-    )
+		</>
+	)
 }
 
 DefaultLayout.propTypes = {
-    children: PropTypes.node.isRequired,
-    bodyClass: PropTypes.string,
-    isHome: PropTypes.bool,
-    data: PropTypes.shape({
-        file: PropTypes.object,
-        allGhostSettings: PropTypes.object.isRequired,
-    }).isRequired,
+	children: PropTypes.node.isRequired,
+	bodyClass: PropTypes.string,
+	isHome: PropTypes.bool,
+	data: PropTypes.shape({
+		file: PropTypes.object,
+		allGhostSettings: PropTypes.object.isRequired,
+	}).isRequired,
 }
 
 const DefaultLayoutSettingsQuery = props => (
-    <StaticQuery
-        query={graphql`
+	<StaticQuery
+		query={graphql`
             query GhostSettings {
                 allGhostSettings {
                     edges {
@@ -147,8 +149,8 @@ const DefaultLayoutSettingsQuery = props => (
                 }
             }
         `}
-        render={data => <DefaultLayout data={data} {...props} />}
-    />
+		render={data => <DefaultLayout data={data} {...props} />}
+	/>
 )
 
 export default DefaultLayoutSettingsQuery
--- src/templates/post.js
+++ src/templates/post.js
@@ -16,117 +16,126 @@ import { MetaData } from '../components/common/meta'
 *
 */
 const Post = ({ data, location }) => {
-    const post = data.ghostPost
+	const post = data.ghostPost
 
-    return (
-        <>
-            <MetaData
-                data={data}
-                location={location}
-                type="article"
-            />
-            <Helmet>
-                <style type="text/css">{`${post.codeinjection_styles}`}</style>
-            </Helmet>
-            <Layout>
-                <div className="container">
-                    <article className="content">
-                        
-                        <header class="post-full-header">
-                        {post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
-                        <h1 className="content-title">{post.title}</h1>
-                        <p class="post-full-custom-excerpt">{post.excerpt}</p>
-                        <div className="post-full-byline-meta">
-                            <div>
-                                <ul class="author-list">
-                                    <li class="author-list-item">
-                                        <div class="author-card">
-                                            {post.primary_author.profile_image ?
-                                                <img className="author-avatar" src={post.primary_author.profile_image} alt={post.primary_author.name}/> :
-                                                <img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name}/>
-                                            }
-                                        </div>
-                                    </li>
-                                </ul>
-                            </div>
-                            <div>
-                                <h4 className="author-name">{ post.primary_author.name }</h4>
-                                <span className="byline-meta-date">{ post.created_at_pretty }</span>
-                            </div>
-                        </div>
-                        </header>
+	return (
+		<>
+			<MetaData
+				data={data}
+				location={location}
+				type="article"
+			/>
+			<Helmet>
+				<style type="text/css">{`${post.codeinjection_styles}`}</style>
+			</Helmet>
+			<Layout>
+				<div className="container content-post">
+					<article className="content">
 
-                        { post.feature_image ?
-                        <figure className="post-feature-image">
-                            <img src={ post.feature_image } alt={ post.title } />
-                        </figure> : null }
+						<header class="post-full-header">
+							{post.tags && <div className="post-card-tags"> <Tags post={post} visibility="public" autolink={false} /></div>}
+							<h1 className="content-title">{post.title}</h1>
+							<p class="post-full-custom-excerpt">{post.excerpt}</p>
+							<div className="post-full-byline-meta">
+								<div>
+									<ul class="author-list">
+										<li class="author-list-item">
+											<div class="author-card">
+												{post.primary_author.profile_image ?
+													<img className="author-avatar" src={post.primary_author.profile_image} alt={post.primary_author.name} /> :
+													<img className="default-avatar" src="/images/icons/avatar.svg" alt={post.primary_author.name} />
+												}
+											</div>
+										</li>
+									</ul>
+								</div>
+								<div>
+									<h4 className="author-name">{post.primary_author.name}</h4>
+									<span className="byline-meta-date">{post.created_at_pretty}</span>
+								</div>
+							</div>
+						</header>
 
-                        <section className="post-full-content">
-                            {/* The main post content */ }
-                            <section
-                                className="content-body load-external-scripts"
-                                dangerouslySetInnerHTML={{ __html: post.html }}
-                            />
-                        </section>
+						{post.feature_image ?
+							<figure className="post-feature-image">
+								<img src={post.feature_image} alt={post.title} />
+							</figure> : null}
 
-                        <section className="post-share">
-                        <InlineShareButtons
-                            config={ {
-                                alignment: 'center',    // alignment of buttons (left, center, right)
-                                color: 'social',      // set the color of buttons (social, white)
-                                enabled: true,        // show/hide buttons (true, false)
-                                font_size: 14,        // font size for the buttons
-                                labels: 'cta',        // button labels (cta, counts, null)
-                                language: 'es',       // which language to use (see LANGUAGES)
-                                networks: [           // which networks to include (see SHARING NETWORKS)
-                                    'facebook',
-                                    'twitter',
-                                    'whatsapp'
-                                ],
-                                padding: 12,          // padding within buttons (INTEGER)
-                                radius: 4,            // the corner radius on each button (INTEGER)
-                                show_total: true,
-                                size: 40,             // the size of each button (INTEGER)
+						<section className="post-full-content">
+							{/* The main post content */}
+							<section
+								className="content-body load-external-scripts"
+								dangerouslySetInnerHTML={{ __html: post.html }}
+							/>
+						</section>
 
-                                // OPTIONAL PARAMETERS
-                                url: post.url, // (defaults to current url)
-                                title: post.title,            // (defaults to og:title or twitter:title)
-                                message: post.excerpt,     // (only for email sharing)
-                                subject: post.title,  // (only for email sharing)
-                                username: 'todoconta' // (only for twitter sharing)
-                            } }
-                        />
-                        </section>
-                        
-                    </article>
-                    <aside className="comments">
-                        <section className="post-comments">
-                            <DiscussionEmbed
-                                shortname='todoconta'
-                                config={ {
-                                    url: post.url,
-                                    identifier: post.id,
-                                    title: post.title,
-                                } }
-                            />
-                        </section>
-                    </aside>
-                </div>
-            </Layout>
-        </>
-    )
+						<section className="post-share">
+							<InlineShareButtons
+								config={{
+									alignment: 'center',    // alignment of buttons (left, center, right)
+									color: 'social',      // set the color of buttons (social, white)
+									enabled: true,        // show/hide buttons (true, false)
+									font_size: 14,        // font size for the buttons
+									labels: 'cta',        // button labels (cta, counts, null)
+									language: 'es',       // which language to use (see LANGUAGES)
+									networks: [           // which networks to include (see SHARING NETWORKS)
+										'facebook',
+										'twitter',
+										'whatsapp'
+									],
+									padding: 12,          // padding within buttons (INTEGER)
+									radius: 4,            // the corner radius on each button (INTEGER)
+									show_total: true,
+									size: 40,             // the size of each button (INTEGER)
+
+									// OPTIONAL PARAMETERS
+									url: post.url, // (defaults to current url)
+									title: post.title,            // (defaults to og:title or twitter:title)
+									message: post.excerpt,     // (only for email sharing)
+									subject: post.title,  // (only for email sharing)
+									username: 'todoconta' // (only for twitter sharing)
+								}}
+							/>
+						</section>
+
+						<section className="comments">
+							<section className="post-comments">
+								<DiscussionEmbed
+									shortname='todoconta'
+									config={{
+										url: post.url,
+										identifier: post.id,
+										title: post.title,
+									}}
+								/>
+							</section>
+						</section>
+					</article>
+					<aside className="sidebar">
+						<section className="suscribe">
+							<h3>Suscríbete</h3>
+							<p>No te pierdas nuestro contenido</p>
+						</section>
+						<section className="post-lists">
+
+						</section>
+					</aside>
+				</div>
+			</Layout>
+		</>
+	)
 }
 
 Post.propTypes = {
-    data: PropTypes.shape({
-        ghostPost: PropTypes.shape({
-            codeinjection_styles: PropTypes.object,
-            title: PropTypes.string.isRequired,
-            html: PropTypes.string.isRequired,
-            feature_image: PropTypes.string,
-        }).isRequired,
-    }).isRequired,
-    location: PropTypes.object.isRequired,
+	data: PropTypes.shape({
+		ghostPost: PropTypes.shape({
+			codeinjection_styles: PropTypes.object,
+			title: PropTypes.string.isRequired,
+			html: PropTypes.string.isRequired,
+			feature_image: PropTypes.string,
+		}).isRequired,
+	}).isRequired,
+	location: PropTypes.object.isRequired,
 }
 
 export default Post
